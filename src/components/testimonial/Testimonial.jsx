7116cdc17f8001a4e577e901b78fdce97c7e0efd
code-washing <tadcassin225@gmail.com>
2023-04-09 00:35:44 +0600

    Start building the reviews section

---
+++ src/components/testimonial/Testimonial.jsx
@@ -0,0 +1,43 @@
+//react
+import { useRef } from 'react';
+
+//styles
+import styles from './Testimonial.module.css';
+
+export default function Testimonial({
+  imageSource = '',
+  personName = '',
+  oneLiner = '',
+  details = '',
+  extraClass = [],
+  extraInlineStyle = {},
+}) {
+  const testimonialRef = useRef();
+
+  return (
+    <div
+      ref={testimonialRef}
+      style={extraInlineStyle}
+      className={`${styles['testimonial']} ${
+        extraClass.length > 0 ? extraClass.join(' ') : 'default class'
+      }`}
+    >
+      <div
+        className={styles['testimonial__img']}
+        style={{ background: `url(${imageSource})` }}
+      ></div>
+
+      <div className={styles['testimonial__text-container']}>
+        <p className={styles['testimonial__text-container__one-liner']}>
+          {`"${oneLiner}"`}
+        </p>
+        <p className={styles['testimonial__text-container__details']}>
+          {details}
+        </p>
+        <p
+          className={styles['testimonial__text-container__person-name']}
+        >{`- ${personName}`}</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/data/TestimonialData.js
@@ -0,0 +1,43 @@
+//image source
+import tamara from "../assets/tamara-bellis-small.webp";
+import richard from "../assets/richard-parker-small.webp";
+import akachi from "../assets/prince-akachi-small.webp";
+import christina from "../assets/christina-lopez-small.webp";
+
+export const testimonialData = [
+  {
+    id: 0,
+    imageSource: richard,
+    oneLiner: "Very professional.",
+    details:
+      "Quia ipsa recusandae sed delectus explicabo consequatur laudantium! Quibusdam.",
+    personName: "Richard Parker",
+    extraClass: [],
+  },
+  {
+    id: 1,
+    imageSource: tamara,
+    oneLiner: "They're amazing!",
+    details: "Voluptatibus maxime dolore consequuntur neque, assumenda.",
+    personName: "Tamara Bellis",
+    extraClass: ["reverse"],
+  },
+  {
+    id: 2,
+    imageSource: christina,
+    oneLiner: "Life became easier.",
+    details:
+      "Possimus nobis esse dignissimos, laboriosam ipsa labore officia, sint.",
+    personName: "Christina Lopez",
+    extraClass: [],
+  },
+  {
+    id: 3,
+    imageSource: akachi,
+    oneLiner: "Excellent staff!",
+    details:
+      "Labore accusamus porro laborum eveniet consequatur explicabo at totam Dignissimo.",
+    personName: "Prince Akachi",
+    extraClass: ["reverse"],
+  },
+];
