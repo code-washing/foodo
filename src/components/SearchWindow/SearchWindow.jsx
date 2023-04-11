a96969f1f8d8d1d0d9341009ccaa265535d9abc2
code-washing <tadcassin225@gmail.com>
2023-04-11 14:34:21 +0600

    Create new component search window to make the app more modular

---
+++ src/components/SearchWindow/SearchWindow.jsx
@@ -0,0 +1,27 @@
+//component
+import Searchbar from '../Searchbar/Searchbar';
+
+//styles
+import styles from './SearchWindow.module.css';
+
+//SearchWindow starts here
+export default function SearchWindow({
+  headerHeight = null,
+  extraClass = undefined,
+}) {
+  // jsx template
+
+  const customStyles = {
+    marginTop: `${headerHeight}px`,
+    height: `calc(100vh - ${headerHeight}px)`,
+  };
+
+  return (
+    <div
+      style={customStyles}
+      className={`${styles['search-window-main']} ${
+        extraClass ? extraClass.join(' ') : 'no-extra-class'
+      }`}
+    ></div>
+  );
+}
