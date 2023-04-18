f65325db3d116b2c685426b8dfb0eea813ed2439
code-washing <tadcassin225@gmail.com>
2023-04-18 17:49:39 +0600

    Create ImageLink component

---
+++ src/components/ImageLink/ImageLink.jsx
@@ -0,0 +1,43 @@
+//components
+import LinkElement from '../LinkElement/LinkElement';
+import TertiaryHeading from '../TertiaryHeading/TertiaryHeading';
+
+//styles
+import styles from './ImageLink.module.css';
+
+//ImageLink starts here
+export default function ImageLink({
+  dataObject = undefined,
+  extraClass = undefined,
+}) {
+  // jsx template
+  return (
+    <div
+      className={`${styles['image-link-main']} ${
+        extraClass ? extraClass.join(' ') : 'no-extra-class'
+      }`}
+    >
+      <LinkElement
+        isText={false}
+        htmlElement={true}
+        toUrl={dataObject.toUrl}
+        linkFor={dataObject.linkFor}
+        extraClass={[styles['image-link-main__link']]}
+      >
+        <div className={styles['image-link-main__container']}>
+          <TertiaryHeading
+            extraClass={[styles['image-link-main__container__title']]}
+            heading={dataObject.title}
+          />
+
+          <div
+            style={{ background: `url(${dataObject.imageSource})` }}
+            className={styles['image-link-main__container__image']}
+          >
+            &nbsp;
+          </div>
+        </div>
+      </LinkElement>
+    </div>
+  );
+}
