cbd287f548a2d184f1d4498a6d1eb0474dd32fdb
code-washing <tadcassin225@gmail.com>
2023-04-18 17:51:55 +0600

    Create Menu page component

--- src/page-components/Menu/Menu.jsx
+++ src/page-components/Menu/Menu.jsx
@@ -1,3 +1,13 @@
+//component
+import HeroSection from '../../section-components/HeroSection/HeroSection';
+import MenuCategorySection from '../../section-components/MenuCategorySection/MenuCategorySection';
+
+//image source
+import HeroImage from '../../assets/menu-page-hero.webp';
+
+//data
+import { foodCategories } from '../../data/FoodCategoryData';
+
 //styles
 import styles from './Menu.module.css';
 
@@ -9,6 +19,23 @@ export default function Menu({ extraClass = undefined }) {
       className={`${styles['menu-main']} ${
         extraClass ? extraClass.join(' ') : 'no-extra-class'
       }`}
-    ></div>
+    >
+      <HeroSection
+        imageSource={HeroImage}
+        shortIntroduction={'Welcome'}
+        heading={'Explore Our Creations: The Perfect Meal Awaits'}
+        buttonText={'View Menu'}
+        toUrl="menu#menu"
+        linkFor="hashed"
+        extraClass={['section-margin']}
+      />
+
+      <MenuCategorySection
+        sectionId={'menu'}
+        sectionTitle={'Menu'}
+        categoryData={foodCategories}
+        extraClass={['section-margin', 'scroll-offset']}
+      />
+    </div>
   );
 }
