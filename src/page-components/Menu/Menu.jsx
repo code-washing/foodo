5f732fd9da2bdd93b9b57d95b686773c17c129de
code-washing <tadcassin225@gmail.com>
2023-04-19 17:02:53 +0600

    Refactor code on menu page

--- src/page-components/Menu/Menu.jsx
+++ src/page-components/Menu/Menu.jsx
@@ -1,6 +1,14 @@
+//react
+import { useEffect } from 'react';
+
+//redux
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllFoodItems } from '../../features/foodItems/foodItemsSlice';
+
 //component
 import HeroSection from '../../section-components/HeroSection/HeroSection';
 import MenuCategorySection from '../../section-components/MenuCategorySection/MenuCategorySection';
+import MenuFoodItemsSection from '../../section-components/MenuFoodItemsSection/MenuFoodItemsSection';
 
 //image source
 import HeroImage from '../../assets/menu-page-hero.webp';
@@ -13,6 +21,12 @@ import styles from './Menu.module.css';
 
 //Menu starts here
 export default function Menu({ extraClass = undefined }) {
+  const dispatch = useDispatch();
+  const allFoodItems = useSelector((state) => state.foodItems.allFoodItems);
+  useEffect(() => {
+    dispatch(getAllFoodItems());
+  }, []);
+
   // jsx template
   return (
     <div
@@ -35,6 +49,12 @@ export default function Menu({ extraClass = undefined }) {
         sectionTitle={'Menu'}
         categoryData={foodCategories}
         extraClass={['section-margin', 'scroll-offset']}
+      />
+
+      <MenuFoodItemsSection
+        allItems={allFoodItems && allFoodItems}
+        categories={foodCategories}
+        extraClass={['section-margin']}
       />
     </div>
   );
