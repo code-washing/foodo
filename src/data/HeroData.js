217d43f4b4b9513e0224c4c60ebf3fd9f0c4d6ba
code-washing <tadcassin225@gmail.com>
2023-04-23 21:18:41 +0600

    Set up data for wide hero section's carousel images

---
+++ src/data/HeroData.js
@@ -0,0 +1,29 @@
+//image source
+import introheroImage from '../assets/introhero.webp';
+import pizzaheroImage from '../assets/pizzahero.webp';
+import steakheroImage from '../assets/steakhero.webp';
+import burgerheroImage from '../assets/burgerhero.webp';
+import soupheroImage from '../assets/souphero.webp';
+
+export const heroImagesArray = [
+  {
+    id: 0,
+    imageSource: introheroImage,
+  },
+  {
+    id: 1,
+    imageSource: pizzaheroImage,
+  },
+  {
+    id: 2,
+    imageSource: burgerheroImage,
+  },
+  {
+    id: 3,
+    imageSource: steakheroImage,
+  },
+  {
+    id: 4,
+    imageSource: soupheroImage,
+  },
+];
