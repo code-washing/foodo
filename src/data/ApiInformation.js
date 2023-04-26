594d227bee246ac3feee13a42e308832538ff9a6
code-washing <tadcassin225@gmail.com>
2023-04-27 01:59:30 +0600

    Delete the api information file from the repo

--- src/data/ApiInformation.js
+++
@@ -1,4 +0,0 @@
-export const foodItemsApi = 'http://localhost:8000/server/foodItemsApi.php';
-export const reviewsApi = 'http://localhost:8000/server/reviewsApi.php';
-export const searchFoodItemsAPI =
-  'http://localhost:8000/server/searchFoodItemsApi.php';
