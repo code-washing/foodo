9f9ced8d87dcaf4bdf50b6097ec5fe794d16083d
code-washing <tadcassin225@gmail.com>
2023-05-05 00:42:42 +0600

    Fix a typo in the promotion message

--- src/data/PromotionInformation.js
+++ src/data/PromotionInformation.js
@@ -6,4 +6,4 @@ export const promotionImage = nascarDriverImage;
 export const promotionHeading = 'Meet Josh Bilicki at the Nascars!';
 
 export const promotionMessage =
-  "We proudly support our team at Nascar. If you're lucky to be the Customer Of The Month you get to meet our racer Josh Bilicki in person at therace track!";
+  "We proudly support our team at Nascar. If you're lucky to be the Customer Of The Month you get to meet our racer Josh Bilicki in person at the race track!";
